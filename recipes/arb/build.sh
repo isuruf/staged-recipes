82cfcc088367eab60715f442836bac165994953c
Isuru Fernando <isuruf@gmail.com>
2016-10-12 09:39:15 +0530

    Add arb

--- recipes/arb/build.sh
+++ recipes/arb/build.sh
@@ -4,9 +4,9 @@ chmod +x configure
 
 # This is set to reduce the number of random tests run so that CIs can run 
 # tests to completion without timeouts.
-echo "int flint_test_multiplier(){return 1;}" > test_helpers.c
+export ARB_TEST_MULTIPLIER=0.1;
 
-./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX
+./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX -with-flint=$PREFIX
 make
-#make check
+make check
 make install
