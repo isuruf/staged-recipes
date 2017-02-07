bf36eae9e7d188d1137a8bcd727ee79d0823d125
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2017-02-07 11:13:29 +0530

    Update build.sh

--- recipes/givaro/build.sh
+++ recipes/givaro/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
-export LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib $LDFLAGS"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export CFLAGS="-fPIC -O2 $CFLAGS"
 export CXXFLAGS="-fPIC -O2 $CXXFLAGS"
 
