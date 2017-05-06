b539a47c6750bde167935465a902b49eae5c4481
Isuru Fernando <isuruf@gmail.com>
2017-05-06 10:38:42 +0530

    Use cc and c++

--- recipes/rpy2/bld.bat
+++ recipes/rpy2/bld.bat
@@ -9,15 +9,15 @@ set CFLAGS=-D__USE_MINGW_ANSI_STDIO
 :: Create an empty library for msvcrt??? since CygwinCCompiler seems to
 :: think that linking to that is a good idea (it is not).
 if "%CONDA_PY%" == "36" (
-  ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
+  ar cru %PREFIX%\libs\libmsvcr140.dll.a
 ) else (
   if "%CONDA_PY%" == "35" (
-    ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
+    ar cru %PREFIX%\libs\libmsvcr140.dll.a
   ) else (
     if "%CONDA_PY%" == "34" (
-      ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr120.dll.a
+      ar cru %PREFIX%\libs\libmsvcr120.dll.a
     ) else (
-      ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr90.dll.a
+      ar cru %PREFIX%\libs\libmsvcr90.dll.a
     )
   )
 )
@@ -25,21 +25,20 @@ if "%CONDA_PY%" == "36" (
 gendef %PREFIX%\python%CONDA_PY%.dll - > python%CONDA_PY%.def
 dlltool -d python%CONDA_PY%.def -l %PREFIX%\lib\libpython%CONDA_PY%.dll.a
 
-copy "%RECIPE_DIR%\build.sh" .
-set PREFIX=%PREFIX:\=/%
-bash -lc "./build.sh"
+set CPATH=%PREFIX%/R/include
+python setup.py install --single-version-externally-managed --record=record.txt
 
 :: Cleanup the libs we made:
-del %CONDA_DEFAULT_ENV%\libs\libpython%CONDA_PY%.dll.a
+del %PREFIX%\libs\libpython%CONDA_PY%.dll.a
 if "%CONDA_PY%" == "36" (
-  del %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
+  del %PREFIX%\libs\libmsvcr140.dll.a
 ) else (
   if "%CONDA_PY%" == "35" (
-    del %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
+    del %PREFIX%\libs\libmsvcr140.dll.a
   ) else (
     if "%CONDA_PY%" == "34" (
-      del %CONDA_DEFAULT_ENV%\libs\libmsvcr120.dll.a
+      del %PREFIX%\libs\libmsvcr120.dll.a
     ) else (
-      del %CONDA_DEFAULT_ENV%\libs\libmsvcr90.dll.a
+      del %PREFIX%\libs\libmsvcr90.dll.a
     )
   )
--- recipes/rpy2/build.sh
+++ recipes/rpy2/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export CC=cc
+export CXX=c++
+cc --version
+c++ --version
+
 # https://github.com/conda-forge/toolchain-feedstock/pull/11
 export CFLAGS="-I${PREFIX}/include -I{PREFIX}/R/include ${CFLAGS}"
 python setup.py install --single-version-externally-managed --record=record.txt
