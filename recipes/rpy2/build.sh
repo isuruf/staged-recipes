a1a92176b783346071f936f7c175283a5befa3c3
Isuru Fernando <isuruf@gmail.com>
2017-05-06 10:28:57 +0530

    More include directories

--- recipes/rpy2/build.sh
+++ recipes/rpy2/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # https://github.com/conda-forge/toolchain-feedstock/pull/11
-export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
+export CFLAGS="-I${PREFIX}/include -I{PREFIX}/R/include ${CFLAGS}"
 python setup.py install --single-version-externally-managed --record=record.txt
