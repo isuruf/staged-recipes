ce79a4515658c140281dc8499e9d5cd587a0eb9e
Isuru Fernando <isuruf@gmail.com>
2017-10-05 16:38:43 -0500

    Build ghc from old ghc

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -1,2 +1,33 @@
-CFLAGS="-L${PREFIX}/lib" ./configure --prefix=$PREFIX
+export GHC_OLD_DIR=`pwd`/binary
+mkdir -p $GHC_OLD_DIR/lib
+
+#export GHC_OLD_VERSION=${PKG_VERSION}
+export GHC_OLD_VERSION="8.2.1"
+
+if [ "$(uname)" == "Linux" ]
+then
+    curl -L -O https://downloads.haskell.org/~ghc/${GHC_OLD_VERSION}/ghc-${GHC_OLD_VERSION}-x86_64-centos67-linux.tar.xz
+    tar -xvf ghc-${GHC_OLD_VERSION}-x86_64-centos67-linux.tar.xz
+elif [ "$(uname)" == "Darwin" ]
+then
+    curl -L -O https://downloads.haskell.org/~ghc/${GHC_OLD_VERSION}/ghc-${GHC_OLD_VERSION}-x86_64-apple-darwin.tar.xz
+    tar -xvf ghc-${GHC_OLD_VERSION}-x86_64-apple-darwin.tar.xz
+else
+    echo "This system is unsupported."
+    exit 1
+fi
+
+
+# Install an older binary version of ghc
+cd ghc-${GHC_OLD_VERSION}
+export LD_LIBRARY_PATH=$GHC_OLD_DIR/lib
+./configure --prefix=$GHC_OLD_DIR
 make install
+cd ..
+
+# Install ghc from source
+export PATH=$PATH:$GHC_OLD_DIR/bin
+./configure --prefix=$PREFIX
+make
+make install
+
