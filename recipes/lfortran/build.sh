62dbc32d1e2371ba6960b69b8a366789a5603997
Ondřej Čertík <ondrej@certik.us>
2019-07-25 22:17:24 -0600

    Use --install-option

--- recipes/lfortran/build.sh
+++ recipes/lfortran/build.sh
@@ -7,9 +7,9 @@ printenv
 if [[ ${MACOSX_DEPLOYMENT_TARGET} != "" ]]; then
     # macOS
     python -m pip install . -vv \
-      --global-option="build" \
-      --global-option="--" \
-      --global-option="-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
+      --install-option="build" \
+      --install-option="--" \
+      --install-option="-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
 else
     # Linux
     python -m pip install . -vv
