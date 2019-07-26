abb98bc91f89f8a164aff8e6992c336ea682d1ee
Isuru Fernando <isuruf@gmail.com>
2019-07-25 21:22:16 -0500

    Update build.sh

--- recipes/lfortran/build.sh
+++ recipes/lfortran/build.sh
@@ -7,9 +7,8 @@ printenv
 if [[ ${MACOSX_DEPLOYMENT_TARGET} != "" ]]; then
     # macOS
     python -m pip install . -vv \
-      --install-option="bdist_wheel" \
-      --install-option="--plat-name" \
-      --install-option="macosx-10.9-x86_64"
+      --install-option="build" \
+      --install-option="-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
 else
     # Linux
     python -m pip install . -vv
