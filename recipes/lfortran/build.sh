a70daaaaa033bbf7fbf762c43bb38103ca645cc2
Ondřej Čertík <ondrej@certik.us>
2019-07-25 18:03:52 -0600

    Add a switch

--- recipes/lfortran/build.sh
+++ recipes/lfortran/build.sh
@@ -4,7 +4,13 @@ set -ex
 
 printenv
 
-python -m pip install . -vv \
-  --install-option="bdist_wheel" \
-  --install-option="--plat-name" \
-  --install-option="macosx-10.9-x86_64"
+if [[ MACOSX_DEPLOYMENT_TARGET != "" ]]; then
+    # macOS
+    python -m pip install . -vv \
+      --install-option="bdist_wheel" \
+      --install-option="--plat-name" \
+      --install-option="macosx-10.9-x86_64"
+else
+    # Linux
+    python -m pip install . -vv
+fi
