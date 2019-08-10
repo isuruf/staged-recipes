9f1f45f77bc954305805cbcf56c126223c44bce4
Isuru Fernando <isuruf@gmail.com>
2019-08-10 14:52:47 +0000

    Fix license file and libtool

--- recipes/libcups/build.sh
+++ recipes/libcups/build.sh
@@ -2,6 +2,7 @@ if [[ "$target_platform" == linux* ]]; then
     export LIBS="-lrt"
 fi
 
+export LIBTOOL=$PREFIX/bin/libtool
 ./configure --prefix=$PREFIX --with-components=core
 make -j${CPU_COUNT}
 #fails due to some network issue
